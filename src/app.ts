f6deaf761bce792e0a6a03883b7b61a6b623774d
angelop47 <pescettoangelo@gmail.com>
2025-12-10 16:32:32 -0300

    feat: mount API routes under /api

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,15 @@ import cors from 'cors';
 
 const app = express();
 
+import routes from './routes';
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api', routes); // Mount all API routes under /api
+
 app.get('/', (req: Request, res: Response) => {
     res.send('API is running...');
 });
