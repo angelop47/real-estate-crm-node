07e04aa2676cde4ea0dd33dd5e5f06455d24f758
angelop47 <pescettoangelo@gmail.com>
2025-12-10 16:27:45 -0300

    feat: Implement controllers for departments.

---
+++ src/controllers/department/getDepartments.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { supabase } from '../../config/supabase';
+
+export const getDepartments = async (req: Request, res: Response) => {
+    try {
+        const { data, error } = await supabase
+            .from('departments')
+            .select('*');
+
+        if (error) {
+            throw error;
+        }
+
+        res.status(200).json(data);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+};
